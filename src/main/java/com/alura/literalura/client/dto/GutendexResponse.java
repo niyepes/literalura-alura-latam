408d31097e5b77b939e77543b1aa5bd99f0dfcd8
Nicolas <nayepesc@udistrital.edu.co>
2026-02-12 09:20:10 -0500

    fix: Se agregó la dependencia de Lombok al POM

--- src/main/java/com/alura/literalura/client/dto/GutendexResponse.java
+++ src/main/java/com/alura/literalura/client/dto/GutendexResponse.java
@@ -1,4 +1,15 @@
 package com.alura.literalura.client.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
+import lombok.*;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class GutendexResponse {
+    public int count;
+    public String next;
+    public String previous;
+    public List<GutendexLibroDTO> results;
 }
